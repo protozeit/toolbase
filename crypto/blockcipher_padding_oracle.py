4512af33e2c2cf61059a51fd445d6f3fd216efe2
protozeit <sjlooaud@gmail.com>
2020-04-21 05:12:51 +0000

    padding oracle

---
+++ crypto/blockcipher_padding_oracle.py
@@ -0,0 +1,46 @@
+from pwn import *
+import sys
+
+r = remote('crypto.chal.csaw.io', 1003)
+
+blength = 0
+
+r.recvline()
+r.sendline('a')
+r.recvline()
+r.recvline()
+
+print '###################################'
+print 'Calculating block length'
+print '  '
+for i in range(0,100):
+     r.sendline('a'*i) 
+     r.recvline()
+     a1 = r.recvline()
+     r.sendline('a'*(i+1))
+     r.recvline()
+     a2 = r.recvline()
+     if a1[:128] == a2[:128]:
+             print "Flag has max length : " + str(i)
+             blength = i
+             break
+
+print "block length is "  + str(blength)
+flag = 'flag{y0u_k'
+
+for k in range(len(flag)+1,blength):
+        pload = 'a'*(blength-k)
+        r.sendline(pload)
+        r.recvline()
+        h1 = r.recvline()[:128]
+
+        for j in range(32,127):
+                r.sendline(pload +flag+ chr(j))
+                r.recvline()
+                hj = r.recvline()[:128]
+
+                if hj[:128] == h1[:128]:
+                        flag += chr(j)
+                        print flag
+                        if '}' in flag: sys.exit()
+                        break
